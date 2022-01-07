71d8ac330d1a35bc95362c59390212ddcbedad3e
xenotux <xenotux@outlook.com>
2022-01-07 20:49:57 +0100

    Update comment

--- include/engine-export.hpp
+++ include/engine-export.hpp
@@ -3,7 +3,7 @@
 
 // Include this header in user code before including any other engine headers,
 // This header defines the export variable to be none which is what the user wants when linking to the engine.
-// Alternatively the symbol can be defined in cmake.
+// Alternatively (More practical) the symbol can be defined in the build files.
 #define MANA_EXPORT
 
 #endif //MANA_ENGINE_EXPORT_HPP
