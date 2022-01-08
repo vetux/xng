432a77caa36e99dc77f149ead1ba52f7897d5731
xenotux <xenotux@outlook.com>
2022-01-08 01:04:53 +0100

    Static link to shaderc_combined

--- cmake/xengine.cmake
+++ cmake/xengine.cmake
@@ -18,8 +18,7 @@ target_include_directories(xengine PUBLIC ${Engine.Dir.SRC})
 target_link_libraries(xengine
         Threads::Threads
         freetype
-        shaderc
-        shaderc_util
+        shaderc_combined
         spirv-cross-core
         spirv-cross-glsl
         spirv-cross-hlsl
