9159c2df98d00c6a82096297632ea5c85ea28d61
xenotux <xenotux@outlook.com>
2022-05-22 18:35:07 +0200

    Add opengl driver check in renderdevice.cpp

--- engine/src/render/platform/renderdevice.cpp
+++ engine/src/render/platform/renderdevice.cpp
@@ -1,12 +1,16 @@
 #include "render/platform/renderdevice.hpp"
 
+#ifdef DRIVER_OPENGL
 #include "render/platform/opengl/oglrenderdevice.hpp"
+#endif
 
 namespace xengine {
     std::unique_ptr<RenderDevice> RenderDevice::create(RenderPlatform backend) {
         switch (backend) {
+#ifdef DRIVER_OPENGL
             case OPENGL_4_1:
                 return std::make_unique<opengl::OGLRenderDevice>();
+#endif
             case OPENGL_4_1_QT:
             case DIRECTX_11:
             case VULKAN:
