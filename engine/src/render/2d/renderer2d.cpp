2cb06dfe1b6393d9e58d26abae525dd5690c19a9
xenotux <xenotux@outlook.com>
2022-07-15 20:01:11 +0200

    Add bone attributes in renderer2d vertex shader

--- engine/src/render/2d/renderer2d.cpp
+++ engine/src/render/2d/renderer2d.cpp
@@ -31,10 +31,12 @@ layout (location = 1) in vec3 normal;
 layout (location = 2) in vec2 uv;
 layout (location = 3) in vec3 tangent;
 layout (location = 4) in vec3 bitangent;
-layout (location = 5) in vec4 instanceRow0;
-layout (location = 6) in vec4 instanceRow1;
-layout (location = 7) in vec4 instanceRow2;
-layout (location = 8) in vec4 instanceRow3;
+layout (location = 5) in ivec4 boneIds;
+layout (location = 6) in vec4 boneWeights;
+layout (location = 7) in vec4 instanceRow0;
+layout (location = 8) in vec4 instanceRow1;
+layout (location = 9) in vec4 instanceRow2;
+layout (location = 10) in vec4 instanceRow3;
 
 layout (location = 0) out vec4 fPosition;
 layout (location = 1) out vec2 fUv;
