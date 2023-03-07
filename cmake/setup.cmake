5f6ba137637492d6cb96a2ebef9b45d66e83589c
xenotux <xenotux@outlook.com>
2023-03-07 18:57:05 +0100

    Add setup.cmake

---
+++ cmake/setup.cmake
@@ -0,0 +1,30 @@
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+find_package(Threads REQUIRED)
+
+set(RELEASE_COMPILER_FLAGS)
+set(DEBUG_COMPILER_FLAGS)
+
+if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    set(RELEASE_COMPILER_FLAGS -O3)
+    if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+        # Optional sanitization flags, breaks debugging
+        # set(DEBUG_COMPILER_FLAGS "${DEBUG_COMPILER_FLAGS} -fsanitize=leak -fsanitize=undefined -fsanitize-address-use-after-scope")
+    endif ()
+endif ()
+
+set(CMAKE_C_FLAGS_RELEASE ${RELEASE_COMPILER_FLAGS})
+set(CMAKE_CXX_FLAGS_RELEASE ${RELEASE_COMPILER_FLAGS})
+set(CMAKE_C_FLAGS_DEBUG ${DEBUG_COMPILER_FLAGS})
+set(CMAKE_CXX_FLAGS_DEBUG  ${DEBUG_COMPILER_FLAGS})
+
+if (UNIX)
+    add_compile_definitions(XENGINE_EXPORT=__attribute__\(\(visibility\("default"\)\)\))
+elseif (WIN32)
+    add_compile_definitions(XENGINE_EXPORT=__declspec\(dllexport\))
+else ()
+    add_compile_definitions(XENGINE_EXPORT=)
+endif ()
