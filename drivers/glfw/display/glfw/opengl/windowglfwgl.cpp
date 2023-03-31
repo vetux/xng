5609f9cef2838d1e3c85f4fd5571c2b2205c9c45
xenotux <xenotux@outlook.com>
2023-03-31 13:50:36 +0200

    Update glfw opengl context version hint

--- drivers/glfw/display/glfw/opengl/windowglfwgl.cpp
+++ drivers/glfw/display/glfw/opengl/windowglfwgl.cpp
@@ -169,8 +169,8 @@ namespace xng::glfw {
 
         glfwDefaultWindowHints();
 
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
         applyHints(attributes);
@@ -207,8 +207,8 @@ namespace xng::glfw {
 
         glfwDefaultWindowHints();
 
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
         applyHints(attributes);
