937ade5d78b134b6f841bad6eb5b50123c26c377
xenotux <xenotux@outlook.com>
2023-06-12 20:28:43 +0200

    Add test common directory, Move assets

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -1,27 +1,28 @@
+set(TESTS_COMMON_DIR ${BASE_SOURCE_DIR}/tests/common/src/)
+set(TESTS_ASSET_DIR ${BASE_SOURCE_DIR}/tests/common/assets)
+
 add_executable(test-framegraph ${BASE_SOURCE_DIR}/tests/framegraph/src/main.cpp)
-target_include_directories(test-framegraph PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/tests/framegraph/src/ ${BASE_SOURCE_DIR}/shaders/generated/)
+target_include_directories(test-framegraph PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/tests/framegraph/src/ ${BASE_SOURCE_DIR}/shaders/generated/ ${TESTS_COMMON_DIR})
 target_link_libraries(test-framegraph Threads::Threads xengine)
 
 add_executable(test-renderer2d ${BASE_SOURCE_DIR}/tests/renderer2d/src/main.cpp)
-target_include_directories(test-renderer2d PRIVATE ${CMAKE_SOURCE_DIR}/tests/renderer2d/src/)
+target_include_directories(test-renderer2d PRIVATE ${CMAKE_SOURCE_DIR}/tests/renderer2d/src/ ${TESTS_COMMON_DIR})
 target_link_libraries(test-renderer2d Threads::Threads xengine)
 
 add_executable(test-canvasrendersystem ${BASE_SOURCE_DIR}/tests/canvasrendersystem/src/main.cpp)
-target_include_directories(test-canvasrendersystem PRIVATE ${CMAKE_SOURCE_DIR}/tests/canvasrendersystem/src/)
+target_include_directories(test-canvasrendersystem PRIVATE ${CMAKE_SOURCE_DIR}/tests/canvasrendersystem/src/ ${TESTS_COMMON_DIR})
 target_link_libraries(test-canvasrendersystem Threads::Threads xengine)
 
 add_executable(test-pak ${BASE_SOURCE_DIR}/tests/pak/src/main.cpp)
-target_include_directories(test-pak PRIVATE ${CMAKE_SOURCE_DIR}/tests/pak/src/)
+target_include_directories(test-pak PRIVATE ${CMAKE_SOURCE_DIR}/tests/pak/src/ ${TESTS_COMMON_DIR})
 target_link_libraries(test-pak Threads::Threads xengine)
 
-file(COPY ${BASE_SOURCE_DIR}/tests/assets DESTINATION ${CMAKE_CURRENT_BINARY_DIR}) # Copy assets
-
 add_executable(test-shaders ${BASE_SOURCE_DIR}/tests/shadertest/src/main.cpp)
-target_include_directories(test-shaders PRIVATE ${CMAKE_SOURCE_DIR}/tests/shadertest/src/)
+target_include_directories(test-shaders PRIVATE ${CMAKE_SOURCE_DIR}/tests/shadertest/src/ ${TESTS_COMMON_DIR})
 target_link_libraries(test-shaders Threads::Threads xengine)
-
-file(COPY ${BASE_SOURCE_DIR}/tests/assets DESTINATION ${CMAKE_CURRENT_BINARY_DIR}) # Copy assets
 
 if (MSVC)
 	target_compile_options(test-canvasrendersystem PUBLIC /bigobj)
 endif ()
+
+file(COPY ${TESTS_ASSET_DIR} DESTINATION ${CMAKE_CURRENT_BINARY_DIR}) # Copy assets
---
+++ tests/common/src/debugoverlay.hpp
@@ -0,0 +1,106 @@
+/**
+ *  xEngine - C++ Game Engine Library
+ *  Copyright (C) 2023  Julian Zampiccoli
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 3 of the License, or (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software Foundation,
+ *  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef XENGINE_DEBUGOVERLAY_HPP
+#define XENGINE_DEBUGOVERLAY_HPP
+
+#include "xng/xng.hpp"
+
+using namespace xng;
+
+class DebugOverlay {
+public:
+    Vec2i pixelSize = {0, 25};
+    Renderer2D &ren2D;
+    TextRenderer textRenderer;
+
+    Text dynText;
+    TextureAtlasHandle dynHandle;
+
+    Text staticText;
+    TextureAtlasHandle staticHandle;
+    bool staticAlloc = false;
+
+    DebugOverlay(Font &font, Renderer2D &renderer2D) : ren2D(renderer2D),
+                                                       textRenderer(font, renderer2D, pixelSize) {}
+
+    void draw(DeltaTime deltaTime,
+              RenderTarget &screen) {
+        if (!staticAlloc) {
+            staticAlloc = true;
+            std::string staticTxt;
+            staticTxt += ren2D.getDevice().getInfo().vendor + " " + ren2D.getDevice().getInfo().renderer + "\n";
+            staticTxt += ren2D.getDevice().getInfo().version;
+
+            TextLayout layout;
+            layout.lineHeight = pixelSize.y;
+
+            staticText = textRenderer.render(staticTxt, layout);
+            staticHandle = ren2D.createTexture(staticText.getImage());
+        }
+
+        auto deltaMs = std::to_string(deltaTime * 1000);
+        auto fps = std::to_string(1 / deltaTime);
+
+        std::stringstream strm;
+        strm << std::fixed << std::setprecision(3) << deltaMs;
+
+        std::string dynTxt;
+        dynTxt += strm.str() + " / ";
+        strm = {};
+
+        strm << std::fixed << std::setprecision(0) << fps;
+
+        dynTxt += strm.str() + "\n";
+
+        dynTxt += "Draws:" + std::to_string(ren2D.getDrawCallCount()) + "\n";
+        dynTxt += "Polys:" + std::to_string(ren2D.getPolyDrawCount()) + "\n";
+
+        TextLayout layout;
+        layout.lineHeight = pixelSize.y;
+
+        dynText = textRenderer.render(dynTxt, layout);
+
+        dynHandle = ren2D.createTexture(dynText.getImage());
+
+        ren2D.resetCounters();
+
+        ren2D.renderBegin(screen, false, {}, {}, screen.getDescription().size, {});
+        ren2D.draw(Rectf({}, dynText.getImage().getSize().convert<float>()),
+                   Rectf({}, dynText.getImage().getSize().convert<float>()),
+                   dynHandle,
+                   {},
+                   0,
+                   xng::NEAREST,
+                   ColorRGBA::yellow());
+
+        ren2D.draw(Rectf({}, staticText.getImage().getSize().convert<float>()),
+                   Rectf({static_cast<float>(screen.getDescription().size.x - staticText.getImage().getSize().x), 0}, staticText.getImage().getSize().convert<float>()),
+                   staticHandle,
+                   {},
+                   0,
+                   xng::NEAREST,
+                   ColorRGBA::yellow());
+        ren2D.renderPresent();
+
+        ren2D.destroyTexture(dynHandle);
+    }
+};
+
+#endif //XENGINE_DEBUGOVERLAY_HPP
