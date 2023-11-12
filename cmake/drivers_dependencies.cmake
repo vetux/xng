8181e15f19e20d05f70dfbd5cb320cc119f23d60
xenotux <xenotux@outlook.com>
2023-11-12 21:41:29 +0100

    Only add submodules when corresponding driver is enabled

--- cmake/drivers_dependencies.cmake
+++ cmake/drivers_dependencies.cmake
@@ -1,29 +1,47 @@
 option(SUBMODULE_DEPENDENCIES "Use the submodules to satisfy driver dependencies" true)
 
 if (SUBMODULE_DEPENDENCIES)
-    add_subdirectory(${CMAKE_SOURCE_DIR}/submodules/glfw/)
+    if (DRIVER_GLFW)
+        add_subdirectory(${CMAKE_SOURCE_DIR}/submodules/glfw/)
+    endif ()
 
-    set(BOX2D_BUILD_UNIT_TESTS OFF CACHE BOOL "" FORCE)
-    set(BOX2D_BUILD_TESTBED OFF CACHE BOOL "" FORCE)
-    add_subdirectory(${CMAKE_SOURCE_DIR}/submodules/box2d/)
+    if (DRIVER_BOX2D)
+        set(BOX2D_BUILD_UNIT_TESTS OFF CACHE BOOL "" FORCE)
+        set(BOX2D_BUILD_TESTBED OFF CACHE BOOL "" FORCE)
+        add_subdirectory(${CMAKE_SOURCE_DIR}/submodules/box2d/)
+    endif ()
 
-    #  add_subdirectory(${CMAKE_SOURCE_DIR}/submodules/bullet3/)
+    if (DRIVER_BULLET3)
+        #  add_subdirectory(${CMAKE_SOURCE_DIR}/submodules/bullet3/)
+    endif ()
 
-    set(LIBTYPE STATIC CACHE STRING "" FORCE)
-    add_subdirectory(${CMAKE_SOURCE_DIR}/submodules/openal-soft/)
+    if (DRIVER_OPENAL)
+        set(LIBTYPE STATIC CACHE STRING "" FORCE)
+        add_subdirectory(${CMAKE_SOURCE_DIR}/submodules/openal-soft/)
+    endif ()
 
-    add_subdirectory(${CMAKE_SOURCE_DIR}/submodules/freetype/)
+    if (DRIVER_FREETYPE)
+        add_subdirectory(${CMAKE_SOURCE_DIR}/submodules/freetype/)
+    endif ()
 
-    add_subdirectory(${CMAKE_SOURCE_DIR}/submodules/assimp/)
+    if (DRIVER_ASSIMP)
+        add_subdirectory(${CMAKE_SOURCE_DIR}/submodules/assimp/)
+    endif ()
 
-    add_subdirectory(${CMAKE_SOURCE_DIR}/submodules/libsndfile/)
+    if (DRIVER_SNDFILE)
+        add_subdirectory(${CMAKE_SOURCE_DIR}/submodules/libsndfile/)
+    endif ()
+
+    if (DRIVER_SPIRVCROSS)
+        add_subdirectory(${CMAKE_SOURCE_DIR}/submodules/SPIRV-Cross/)
+    endif ()
+
+    if (DRIVER_CRYPTOPP)
+        add_subdirectory(${CMAKE_SOURCE_DIR}/submodules/cryptopp/)
+    endif ()
 
     set(ENABLE_OPT OFF CACHE BOOL "" FORCE)
     add_subdirectory(${CMAKE_SOURCE_DIR}/submodules/glslang/)
-
-    add_subdirectory(${CMAKE_SOURCE_DIR}/submodules/SPIRV-Cross/)
-
-    add_subdirectory(${CMAKE_SOURCE_DIR}/submodules/cryptopp/)
 
     include_directories(${CMAKE_SOURCE_DIR}/submodules/glm/)
 endif ()
