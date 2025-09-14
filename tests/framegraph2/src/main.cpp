7fed72b1ec03a05c01f187187ce85b6f82201a8a
vetux <xenotux@outlook.com>
2025-09-14 18:08:07 +0200

    Update framegraph2 test

--- tests/framegraph2/src/main.cpp
+++ tests/framegraph2/src/main.cpp
@@ -20,6 +20,8 @@
 #include "xng/adapters/glfw/glfw.hpp"
 #include "xng/adapters/opengl/opengl.hpp"
 
+#include "xng/render/2d/renderpass2d.hpp"
+
 #include "shadertestpass.hpp"
 
 int main(int argc, char *argv[]) {
@@ -30,15 +32,47 @@ int main(int argc, char *argv[]) {
 
     runtime.setWindow(*window);
 
-    RenderGraphBuilder builder;
+    // Print shader test pass
+    {
+        RenderGraphBuilder builder;
+        ShaderTestPass pass;
+        pass.setup(builder);
 
-    ShaderTestPass pass;
-    pass.setup(builder);
+        const auto graph = builder.build();
+        const auto gh = runtime.compile(graph);
 
-    const auto graph = builder.build();
-    const auto gh = runtime.compile(graph);
+        runtime.execute(gh);
+        runtime.destroy(gh);
+    }
 
-    runtime.execute(gh);
+    RenderGraphBuilder builder = {};
+
+    RenderPass2D pass2D;
+    pass2D.setup(builder);
+
+    auto graph = builder.build();
+    auto gh = runtime.compile(graph);
+
+    auto &ren2D = pass2D.getRenderer2D();
+
+    while (!window->shouldClose()) {
+        window->update();
+
+        auto fbSize = window->getFramebufferSize();
+
+        ren2D.renderBegin(fbSize, ColorRGBA::olive());
+        ren2D.draw(Vec2f(0, 0), fbSize.convert<float>(), ColorRGBA::green());
+        ren2D.renderPresent();
+
+        if (pass2D.shouldRebuild()) {
+            builder = {};
+            pass2D.setup(builder);
+            graph = builder.build();
+            runtime.recompile(gh, graph);
+        }
+
+        runtime.execute(gh);
+    }
 
     return 0;
 }
