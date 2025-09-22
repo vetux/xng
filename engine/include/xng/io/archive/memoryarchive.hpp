7129aa26da3614b70be5e1acc483ec96a4bbbf52
vetux <xenotux@outlook.com>
2025-09-22 08:55:28 +0200

    Update copyright date

--- engine/include/xng/io/archive/memoryarchive.hpp
+++ engine/include/xng/io/archive/memoryarchive.hpp
@@ -1,6 +1,6 @@
 /**
  *  xEngine - C++ Game Engine Library
- *  Copyright (C) 2024  Julian Zampiccoli
+ *  Copyright (C) 2025 Julian Zampiccoli
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
