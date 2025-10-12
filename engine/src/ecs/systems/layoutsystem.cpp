a306ce63fed83d863850c0e0f49b6ba9cf3b2018
vetux <vetux.github@outlook.com>
2025-10-12 11:54:08 +0200

    Add layout system definitions

---
+++ engine/src/ecs/systems/layoutsystem.cpp
@@ -0,0 +1,37 @@
+/**
+ *  xEngine - C++ Game Engine Library
+ *  Copyright (C) 2025 Julian Zampiccoli
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 3 of the License, or (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software Foundation,
+ *  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "xng/ecs/systems/layoutsystem.hpp"
+
+namespace xng {
+    LayoutSystem::LayoutSystem() {
+    }
+
+    void LayoutSystem::update(DeltaTime deltaTime, EntityScene &scene, EventBus &eventBus) {
+        System::update(deltaTime, scene, eventBus);
+    }
+
+    void LayoutSystem::start(EntityScene &scene, EventBus &eventBus) {
+        System::start(scene, eventBus);
+    }
+
+    void LayoutSystem::stop(EntityScene &scene, EventBus &eventBus) {
+        System::stop(scene, eventBus);
+    }
+}
