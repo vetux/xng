46a88e59d84717f5c03afa7c0c2c433bee6d30da
vetux <vetux.github@outlook.com>
2026-04-06 13:57:41 +0200

    Cleanup transform.hpp, Move comment

--- engine/include/xng/math/transform.hpp
+++ engine/include/xng/math/transform.hpp
@@ -20,101 +20,92 @@
 #ifndef XENGINE_TRANSFORM_HPP
 #define XENGINE_TRANSFORM_HPP
 
-#include "vector3.hpp"
-#include "quaternion.hpp"
-#include "matrixmath.hpp"
+#include "xng/math/vector3.hpp"
+#include "xng/math/quaternion.hpp"
+#include "xng/math/matrixmath.hpp"
 
 #include "xng/io/message.hpp"
 
 namespace xng {
-    /**
-     * TODO: NDC Independent transform coordinate space
-     */
-    class XENGINE_EXPORT Transform : public Messageable {
+    class Transform final : public Messageable {
     public:
         Transform() {
-            mModel = MatrixMath::translate(mPosition) * mRotation.matrix() * MatrixMath::scale(mScale);
+            updateModelMatrix();
         }
 
-        Transform(Vec3f position, Vec3f rotation, Vec3f scale) : mPosition(position),
-                                                                 mRotation(rotation),
-                                                                 mScale(scale) {
-            mModel = MatrixMath::translate(mPosition) * mRotation.matrix() * MatrixMath::scale(mScale);
+        Transform(Vec3f position, const Vec3f &rotation, Vec3f scale)
+            : mPosition(std::move(position)),
+              mRotation(rotation),
+              mScale(std::move(scale)) {
+            updateModelMatrix();
         }
 
-        Transform(Vec3f position, Quaternion rotation, Vec3f scale) : mPosition(position),
-                                                                      mRotation(rotation),
-                                                                      mScale(scale) {
-            mModel = MatrixMath::translate(mPosition) * mRotation.matrix() * MatrixMath::scale(mScale);
-        }
-
-        Transform &operator+=(const Transform &other) {
-            mPosition += other.mPosition;
-            mRotation = other.mRotation * mRotation;
-            mScale += other.mScale;
-            mModel = MatrixMath::translate(mPosition) * mRotation.matrix() * MatrixMath::scale(mScale);
-            return *this;
-        }
-
-        const Mat4f &model() const {
-            return mModel;
+        Transform(Vec3f position, Quaternion rotation, Vec3f scale)
+            : mPosition(std::move(position)),
+              mRotation(std::move(rotation)),
+              mScale(std::move(scale)) {
+            updateModelMatrix();
         }
 
         void setPosition(Vec3f position) {
             if (mPosition == position) return;
             mPosition = std::move(position);
-            mModel = MatrixMath::translate(mPosition) * mRotation.matrix() * MatrixMath::scale(mScale);
+            updateModelMatrix();
         }
 
-        const Vec3f &getPosition() const {
+        [[nodiscard]] const Vec3f &getPosition() const {
             return mPosition;
         }
 
         void setRotation(const Quaternion &quaternion) {
             if (mRotation == quaternion) return;
             mRotation = quaternion;
-            mModel = MatrixMath::translate(mPosition) * mRotation.matrix() * MatrixMath::scale(mScale);
+            updateModelMatrix();
         }
 
-        const Quaternion &getRotation() const {
+        [[nodiscard]] const Quaternion &getRotation() const {
             return mRotation;
+        }
+
+        void setScale(const Vec3f &scale) {
+            if (mScale == scale) return;
+            mScale = scale;
+            updateModelMatrix();
+        }
+
+        [[nodiscard]] const Vec3f &getScale() const {
+            return mScale;
+        }
+
+        [[nodiscard]] const Mat4f &model() const {
+            return mModel;
+        }
+
+        [[nodiscard]] Vec3f forward() const {
+            return MatrixMath::inverse(mRotation.matrix()) * (Vec3f(0, 0, 1));
+        }
+
+        [[nodiscard]] Vec3f up() const {
+            return MatrixMath::inverse(mRotation.matrix()) * (Vec3f(0, 1, 0));
+        }
+
+        [[nodiscard]] Vec3f left() const {
+            return MatrixMath::inverse(mRotation.matrix()) * (Vec3f(-1, 0, 0));
         }
 
         /**
          * Apply the given quaternion rotation to the existing rotation.
          *
          * @param quaternion
-         * @param localAxis If true apply the rotation relative to the world origin rotation, else rotate relative to the current rotation.
+         * @param worldAxis If true, apply the rotation relative to the world origin rotation, else rotate relative to the current rotation.
          */
-        void applyRotation(const Quaternion &quaternion, bool worldAxis = false) {
+        void applyRotation(const Quaternion &quaternion, const bool worldAxis = false) {
             if (worldAxis) {
                 mRotation = mRotation * quaternion;
             } else {
                 mRotation = quaternion * mRotation;
             }
-            mModel = MatrixMath::translate(mPosition) * mRotation.matrix() * MatrixMath::scale(mScale);
-        }
-
-        void setScale(const Vec3f &scale) {
-            if (mScale == scale) return;
-            mScale = scale;
-            mModel = MatrixMath::translate(mPosition) * mRotation.matrix() * MatrixMath::scale(mScale);
-        }
-
-        const Vec3f &getScale() const {
-            return mScale;
-        }
-
-        Vec3f forward() const {
-            return MatrixMath::inverse(mRotation.matrix()) * (Vec3f(0, 0, 1));
-        }
-
-        Vec3f up() const {
-            return MatrixMath::inverse(mRotation.matrix()) * (Vec3f(0, 1, 0));
-        }
-
-        Vec3f left() const {
-            return MatrixMath::inverse(mRotation.matrix()) * (Vec3f(-1, 0, 0));
+            updateModelMatrix();
         }
 
         bool operator==(const Transform &other) const {
@@ -125,13 +116,21 @@ namespace xng {
             return !(*this == other);
         }
 
+        Transform &operator+=(const Transform &other) {
+            mPosition += other.mPosition;
+            mRotation = other.mRotation * mRotation;
+            mScale += other.mScale;
+            updateModelMatrix();
+            return *this;
+        }
+
         Messageable &operator<<(const Message &message) override {
             mPosition << message.getMessage("position");
             Vec3f euler;
             euler << message.getMessage("rotation");
             mRotation = Quaternion(euler);
             mScale << message.getMessage("scale");
-            mModel = MatrixMath::translate(mPosition) * mRotation.matrix() * MatrixMath::scale(mScale);
+            updateModelMatrix();
             return *this;
         }
 
@@ -144,10 +143,15 @@ namespace xng {
         }
 
     private:
-        Mat4f mModel;
+        void updateModelMatrix() {
+            mModel = MatrixMath::translate(mPosition) * mRotation.matrix() * MatrixMath::scale(mScale);
+        }
+
         Vec3f mPosition;
         Quaternion mRotation;
         Vec3f mScale = Vec3f(1);
+
+        Mat4f mModel;
     };
 }
 
